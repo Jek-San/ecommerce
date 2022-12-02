1790b4f74218e4a956baf24eaccaacba196ba5ea
Jek-San <ihsanzack@gmail.com>
2022-12-02 19:22:16 +0700

    default template

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Testimoni from './components/testimoni/Testimoni'
-import Footer from './components/footer/Footer'
+import React from "react"
+import Header from "./components/header/Header"
 
 const App = () => {
   return (
     <>
       <Header />
-      <Nav />
-      <About />
-      <Experience />
-      <Services />
-      <Portfolio />
-      <Testimoni />
-      <Contact />
-      <Footer/>
     </>
   )
 }
